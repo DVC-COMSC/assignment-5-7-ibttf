ceff7f9c578ea0684e4c52a8e96f075d29a84d25
roylee0912 <leeroy0912@gmail.com>
2024-03-08 16:33:00 -0800

    done

--- main.cpp
+++ main.cpp
@@ -8,6 +8,12 @@ int main()
 
 	cin >> n1 >> n2;
 	cin >> N;
+	cout << n1 << n2;
+	for (int i = 3; i<N; i++){
+		n1 = n2;
+		n2 = n1 + n2;	
+		cout << n2 << " ";
+	}
 
 	/***************************************************
 	 * Code your program here
